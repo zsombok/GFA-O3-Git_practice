8f14ab70c4900e2b03b4fa075f1a8a168dca10b1
lendo <lendoo73@gmail.com>
2022-04-11 16:55:55 +0100

    d infinite

--- src/Horoscope.java
+++ src/Horoscope.java
@@ -3,5 +3,6 @@ public class Horoscope {
     public static void future(String zodiac, String month) {
         System.out.println(zodiac);
         System.out.println(month);
+        System.out.println();
     }
 }
